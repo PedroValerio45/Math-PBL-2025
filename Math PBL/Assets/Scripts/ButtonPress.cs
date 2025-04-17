41a5f2bc5d810382b90b951742841892b25249cb
PedroValerio45 <opedrovalerio@gmail.com>
2025-04-17 02:01:36 +0100

    extra camera angle, new UI element, small changes, and removed "it"
    we know what "it" was

--- Math PBL/Assets/Scripts/ButtonPress.cs
+++ Math PBL/Assets/Scripts/ButtonPress.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 
 public class ButtonPress : MonoBehaviour
 {
+    private Camera cam;
     public GameObject button;
     private Animator anim;
+    
     private bool animIsPlaying;
     public static bool buttonIsPressed;
+    [SerializeField] private bool cameraChanged;
+    private Vector3 defaultCameraPosition;
+    private Quaternion defaultCameraRotation;
 
     void Start()
     {
         anim = button.GetComponent<Animator>();
+        cam = Camera.main;
+        
+        defaultCameraPosition = cam.transform.position;
+        defaultCameraRotation = cam.transform.rotation;
     }
 
     private void Update()
@@ -35,6 +44,22 @@ public class ButtonPress : MonoBehaviour
 
         // Reset scene because why not
         if (Input.GetKeyDown(KeyCode.R)) { Application.LoadLevel(Application.loadedLevel); }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            if (!cameraChanged)
+            {
+                cameraChanged = true;
+                cam.transform.position = new Vector3(16.3f, 4.3f, -2.5f);
+                cam.transform.rotation = Quaternion.Euler(20, -80, 0);
+            }
+            else
+            {
+                cameraChanged = false;
+                cam.transform.position = defaultCameraPosition;
+                cam.transform.rotation = defaultCameraRotation;
+            }
+        }
     }
 
     public void AnimFinish()
